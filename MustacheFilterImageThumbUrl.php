9e3657a648d871e2be89182257a4ae35d94e6351
salmi3d <salmi3d@gmail.com>
2019-12-18 12:24:57 +0200

    Fixed bug with type cast of width and height in MustacheFilterImageThumbUrl class

--- MustacheFilterImageThumbUrl.php
+++ MustacheFilterImageThumbUrl.php
@@ -11,9 +11,9 @@ class MustacheFilterImageThumbUrl {
         if (!preg_match('~^([0-9]+)([x|X])([0-9]+)$~', $property, $tmp)) {
             return null;
         }
-        $width = $tmp[1];
+        $width = (int) $tmp[1];
         $crop = ($tmp[2] === 'X');
-        $height = $tmp[3];
+        $height = (int) $tmp[3];
 
         return function($name) use ($width, $height, $crop) {
             // your image resize implementation there that return image thumbnail
